3487d22268ed7fe011ca8982417429b24c27c735
25x8 <kocherginji@gmail.com>
2024-06-04 13:39:29 +0700

    ADD: Withdraw-native and withdraw-token hardhat tasks; Task params simplified, some vars moved to hardhat vars storage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,22 +3,24 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-verify";
 
 import { donationAbi } from "./donationAbi"
+import { parseEther } from "viem";
 
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const DONATION_CONTRACT_ADDRESS = vars.get("DONATION_CONTRACT_ADDRESS") as `0x${string}`
+const DEPLOYER_WALLET_ADDRESS = vars.get("DEPLOYER_WALLET_ADDRESS") as `0x${string}`
 
 task("add-token", "Adds new allowed token to donation contract")
-.addParam("contract", "Token contract address")
+  .addParam("contract", "Token contract address")
   .addParam("symbol", "Token symbol")
   .addParam("decimals", "Token decimals")
-  .addParam("address", "Address of donation smart contract")
-  .setAction(async ({ address, decimals, symbol, contract }, hre) => {
-    const walletClient = await hre.viem.getWalletClient("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+  .setAction(async ({ decimals, symbol, contract }, hre) => {
+    const walletClient = await hre.viem.getWalletClient(DEPLOYER_WALLET_ADDRESS)
     const publicClient = await hre.viem.getPublicClient()
 
     // Simulate request and sign tx
     const { request } = await publicClient.simulateContract({
-      address,
       abi: donationAbi,
+      address: DONATION_CONTRACT_ADDRESS,
       account: walletClient.account,
       functionName: 'addToken',
       args: [symbol, decimals, contract],
@@ -28,18 +30,56 @@ task("add-token", "Adds new allowed token to donation contract")
 
 task("delete-token", "Deletes allowed token from donation contract")
   .addParam("symbol", "Token symbol")
-  .addParam("address", "Address of donation smart contract")
-  .setAction(async ({ symbol, address }, hre) => {
-    const walletClient = await hre.viem.getWalletClient("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+  .setAction(async ({ symbol }, hre) => {
+    const walletClient = await hre.viem.getWalletClient(DEPLOYER_WALLET_ADDRESS)
     const publicClient = await hre.viem.getPublicClient()
 
     // Simulate request and sign tx
     const { request } = await publicClient.simulateContract({
       abi: donationAbi,
+      address: DONATION_CONTRACT_ADDRESS,
       account: walletClient.account,
-      address: address,
       functionName: 'deleteToken',
       args: [symbol],
+    })
+    await walletClient.writeContract(request)
+  });
+
+task("withdraw-native", "Transfers tips left by users in Ether to a specified address")
+  .addParam("to", "Receiver address")
+  .addParam("amount", "Amount in ether")
+  .setAction(async ({ to, amount }, hre) => {
+    const walletClient = await hre.viem.getWalletClient(DEPLOYER_WALLET_ADDRESS)
+    const publicClient = await hre.viem.getPublicClient()
+    const amountWei = parseEther(amount)
+
+    // Simulate request and sign tx
+    const { request } = await publicClient.simulateContract({
+      abi: donationAbi,
+      address: DONATION_CONTRACT_ADDRESS,
+      account: walletClient.account,
+      functionName: 'withdrawNativeToken',
+      args: [to, amountWei],
+    })
+    await walletClient.writeContract(request)
+  });
+
+task("withdraw-token", "Transfers tips left by users in ERC20 token to a specified address")
+  .addParam("symbol", "Token symbol")
+  .addParam("to", "Receiver address")
+  .addParam("amount", "Amount")
+  .setAction(async ({ symbol, to, amount }, hre) => {
+    const walletClient = await hre.viem.getWalletClient(DEPLOYER_WALLET_ADDRESS)
+    const publicClient = await hre.viem.getPublicClient()
+    const amountWei = parseEther(amount)
+
+    // Simulate request and sign tx
+    const { request } = await publicClient.simulateContract({
+      abi: donationAbi,
+      address: DONATION_CONTRACT_ADDRESS,
+      account: walletClient.account,
+      functionName: 'withdrawToken',
+      args: [symbol, to, amountWei],
     })
     await walletClient.writeContract(request)
   });
